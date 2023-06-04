86457692bd515424087572555eae95647ce20e3f
Anchal yadav <anchalyadav2344@gmail.com>
2023-06-05 01:39:42 +0530

    Committing changes in coral-mongo submodule

---
+++ src/Component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Card from '../Component/Card'
+import Table from '../Component/Table'
+// import { BrowserRouter as Router, Route,Switch, Redirect } from 'react-router-dom'
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Card/>
+      <Table/>
+    </>
+  )
+}
+
+export default Home
